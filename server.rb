38bdff117c01c3857672adda3df3972169a0cd93
Derek Hopper <dr.ocktopi@gmail.com>
2013-10-31 20:38:17 -0400

    The battle has begun

--- server.rb
+++ server.rb
@@ -3,3 +3,10 @@ require "sinatra"
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
+
+post '/battles' do
+  combatant1 = params[:combatant1]
+  combatant2 = params[:combatant2]
+  winner = [combatant1, combatant2][rand(2)]
+  return "A close battle! #{winner} is triumphant!"
+end
